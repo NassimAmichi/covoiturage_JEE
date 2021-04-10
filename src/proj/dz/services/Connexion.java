b68aaacca0d842169d1973a34f3dc91bbb8dbfbb
nassim <nassim.amichi.info@gmail.com>
2021-04-10 20:21:08 +0100

    sauvegarde local du 10-04

--- src/proj/dz/services/Connexion.java
+++ src/proj/dz/services/Connexion.java
@@ -56,7 +56,7 @@ public class Connexion  extends Application
  
         num_user2= annonceurDao.connexion(passwords_user[0],get_SHA_512_SecurePassword(passwords_user[1],"tt")); 
         
-        System.out.println("connnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn_poste nouvell");
+        System.out.println("connnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn_poste nouvelle");
         
 		return 	num_user2;
 	}
